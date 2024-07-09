72e668c650ecacc6f4dd4eb8090a84205c05fbd7
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2024-07-09 21:13:08 +0100

    update on ItemLists component

--- src/components/ItemLists.jsx
+++ src/components/ItemLists.jsx
@@ -1,40 +1,28 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
-
+import ItemList from './ItemList';
 import propTypes from 'prop-types';
 
-const ItemLists = ({items, deleteBtnClick, doneBtnClick}) => {
-
+const ItemLists = ({ items, deleteBtnClick, doneBtnClick }) => {
   return (
     <ul className="list-group">
-        {items.map((item) =>{
-            return (
-                    <li className="list-group-item" key={item.id}>
-                        <div className="d-flex justify-content-between align-items-center w-100">
-                        <span className="">{item.name}</span>
-                        <div className="d-flex align-items-center">
-                            <div className="vr me-2 ms-2"></div>
-                            <button className="btn btn-xs btn-danger" onClick={()=> deleteBtnClick(item.id)}>
-                            <FontAwesomeIcon icon={faTrash} />
-                            </button>
-                            <div className="vr me-2 ms-2"></div>
-                            <button className={`btn btn-xs ${item.status? 'btn-warning disabled':'btn-success'}`} onClick={() => doneBtnClick(item.id)}>
-                            <FontAwesomeIcon icon={faCheck} />
-                            </button>
-                        </div>
-                        </div>
-                    </li>
-                )
-            })}
+      {
+        items.length === 0 ? <li className="list-group-item text-center"><h5>No items</h5></li>
+        : 
+        items.map(item => (
+        <ItemList
+          key={item.id}
+          item={item}
+          deleteBtnClick={deleteBtnClick}
+          doneBtnClick={doneBtnClick}
+        />
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 ItemLists.propTypes = {
-    items: propTypes.array.isRequired,
-    deleteBtnClick: propTypes.func.isRequired,
-    doneBtnClick: propTypes.func.isRequired
-}
+  items: propTypes.array.isRequired,
+  deleteBtnClick: propTypes.func.isRequired,
+  doneBtnClick: propTypes.func.isRequired,
+};
 
-
-export default ItemLists
+export default ItemLists;
