09c65f66f1b8d447a22fcb3918359107e5d2a0ed
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2024-07-09 21:12:55 +0100

    ItemList component update

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,24 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
 
+import propTypes from 'prop-types';
 
-import {PropTypes} from 'prop-types';
-
-export const ItemList = ({item}) => {
-  return (
+const ItemList = ({item, deleteBtnClick, doneBtnClick}) => {
+    // if item not null then return the following JSX
+    return (
         <li className="list-group-item" key={item.id}>
             <div className="d-flex justify-content-between align-items-center w-100">
-            <span className="">{item.name}</span>
-            <div className="d-flex align-items-center">
-                <div className="vr me-2 ms-2"></div>
-                <button className="btn btn-xs btn-danger">
-                <FontAwesomeIcon icon={faTrash} />
-                </button>
-                <div className="vr me-2 ms-2"></div>
-                <button className="btn btn-xs btn-success">
-                <FontAwesomeIcon icon={faCheck} />
-                </button>
-            </div>
+                <span className="">{item.name}</span>
+                <div className="d-flex align-items-center">
+                    <div className="vr me-2 ms-2"></div>
+                    <button className="btn btn-xs btn-danger" onClick={() => deleteBtnClick(item.id)}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                    <div className="vr me-2 ms-2"></div>
+                    <button className={`btn btn-xs ${item.status ? 'btn-warning disabled' : 'btn-success'}`} onClick={() => doneBtnClick(item.id)}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </button>
+                </div>
             </div>
         </li>
-        )
+    )
 }
+
+ItemList.propTypes = {
+    item: propTypes.shape({
+        id: propTypes.number.isRequired,
+        name: propTypes.string.isRequired,
+        status: propTypes.bool.isRequired
+    }).isRequired,
+    deleteBtnClick: propTypes.func.isRequired,
+    doneBtnClick: propTypes.func.isRequired
+}
+
+export default ItemList
 
