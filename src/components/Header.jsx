bddc3f585be74c8568c7d24cb7987dea2f3dabf1
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2024-07-10 13:15:52 +0100

    update on Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
+import PropsType from 'prop-types'
 
 const Header = ({header}) => {
   return (
     <h4 className="text-center mt-5 mb-1">{header}</h4>
   )
+}
+
+Header.propTypes = {
+  header: PropsType.string.isRequired
 }
 
 export default Header
